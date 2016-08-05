394394e71ebd288d722df8aaafb8f06d2429d422
Nadyan <nadyan_sp@hotmail.com>
2016-08-04 21:48:37 -0300

    para calcular a complexidade

---
+++ InteirosEmVetor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int *vetor;
+    int tam, qual, qtd = 0, i;
+
+    printf("\n\n    Informe a quantidade de inteiros: ");
+    scanf("%d", &tam);
+
+    vetor = malloc(sizeof(int) * tam);
+
+    printf("\n    Informe quais sao os %d inteiros:\n", tam);
+
+    for(i = 0; i < tam; i++){
+        scanf("%d", &vetor[i]);
+    }
+
+    printf("\n\n    Informe o inteiro a ser contado: ");
+    scanf("%d", &qual);
+
+    for(i = 0; i < tam; i++){
+        if(vetor[i] == qual){
+            qtd++;
+        }
+    }
+
+    printf("\n\n    O inteiro %d foi encontrado %d vezes!\n\n", qual, qtd);
+
+    return 0;
+}
